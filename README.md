a997205fcaebd1650356dd065f1daf7e82fbc409
EDWIN <dji.dimasur.edwin@gmail.com>
2025-03-12 17:22:42 -0600

    creamos el branch de desarrollo y actualizamos el Challengue, creamos a funcion para agregar amigos

--- README.md
+++ README.md
@@ -5,3 +5,5 @@ Creamos nuestro propio repositorio remoto donde creamos los archivos nesearios,
 el .js, .m, .css y .html
 
 Paso 1. Debemos tener lo nesesario para el proyecto, asi que copiamos el estilo y el disemo de la pagina web. y guardamos los cambios en el repositorio remoto, ya que nuestro repositorio remoto esta en ceros.
+
+paso 2. Creamos una lista en la cual guardaremos los nombres ingresados, tambien creamos una funcion para agregar los nombres a la lista, verificando que no sean espacios en blanco.
--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Aqui colocaremos el codigo de javascript para el Challengue
+
+//creamos una lista para almacenar los nombres
+let amigos = [];
+
+//creamos una funcion para agregar amigos a la lista
+function agregarAmigo(){
+    if (document.getElementById('amigo').value != ''){ //condicionamos que el texto no este vacio
+        amigos.push(document.getElementById('amigo').value); //agregamos nombres de amigos a la lista
+        //alert(document.getElementById('amigo').value);
+        console.log(amigos); //mostramos la lista en la consola
+        limpiarCaja();
+    }
+    else{
+        alert("Por favor, ingrese un nombre");   //si el texto ingresado esta vacio no se ejecuta nada y no se guarda nada
+    }
+}
+
+function limpiarCaja(){
+    document.querySelector('#amigo').value = '';
+}
