9c12bf9d5d8a76bf44ca755a484493e6e1064170
EDWIN <dji.dimasur.edwin@gmail.com>
2025-03-13 15:36:53 -0600

    eliminamos errores en la funcion actualizarlista

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ function agregarAmigo(){
         //alert(document.getElementById('amigo').value);
         console.log(amigos); //mostramos la lista en la consola
         limpiarCaja();
+        actualizarListaHTML() //llamaos la funcion para mostrar los nombres en la lista
     }
     else{
         alert("Por favor, ingrese un nombre");   //si el texto ingresado esta vacio no se ejecuta nada y no se guarda nada
@@ -22,22 +23,22 @@ function limpiarCaja(){
 
 //creamos una funcion para actualizar la lista de amigos en html
 function actualizarListaHTML(){
-    let listaAmigoUl = document.getElementById("listaAmigos");
-    listaAmigoUl.innerHTML = "";
-
-    amigos.array.forEach(Element => {
-        let li = document.createElement("li");
-        li.texContent = Element;
-        listaAmigoUl.apeendChild(li);
-    });
+    let listaAmigos = document.getElementById('listaAmigos'); //obteniendo el dato de 'listaAmigos' y guaradando en la variable
+    listaAmigos.innerHTML = "";     // limpiando a lista en html
+    for( let cont = 0; cont < amigos.length; cont++){  //creamos un ciclo para recorrer los datos de nuestro arreglo
+        let element = amigos[cont];  //creamos un elemento en donde guardamos el dato del contador
+        let listaHTML = document.createElement("li"); //creamos la lista 
+        listaHTML.textContent = element;   // el elemento de la listahtml sera igual al elemente que es el nombre
+        listaAmigos.appendChild(listaHTML) //agregamos el elemento de la lista a la lista de amigos
+    }
 }  
-
+//creamos la funcion para sortear los nombres de los amigos
 function sortearAmigo(){
-    let amigoAleatorio = Math.floor(Math.random()*amigos.length);
-    let amigoElejido = amigos[amigoAleatorio];
-    let resultadoU1 = document.getElementById("resultado");
-    resultadoU1.innerHTML = `<li> Tu amigo secreto es!! ${amigoElejido}<li>`
-    if(amigos.length == 0){
+    let amigoAleatorio = Math.floor(Math.random()*amigos.length); //definimos un lugar aleatorio dentro de nuestro arreglo
+    let amigoElejido = amigos[amigoAleatorio]; //definimos el resultado elejido ahora con el valor de la posicion
+    let resultadoU1 = document.getElementById("resultado"); //definimos que el resultado sera escrito en el id resultado
+    resultadoU1.innerHTML = `<li> Tu amigo secreto es!! ${amigoElejido}<li>`  //colocamos el texto y lo agregamos
+    if(amigos.length == 0){        //condicionamos que si la celda esta vacia no podamos ejecutar el sorteo.
         alert("No hay amigos para sortear. Agrega nombres!");
     }
     
